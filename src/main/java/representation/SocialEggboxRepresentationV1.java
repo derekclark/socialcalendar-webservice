04591956f42ddad09b0495fa4a9237143d61d0b5
derek clark <derekclark14@googlemail.com>
2016-06-19 10:19:27 +0100

    returns 404 and empty body if user not found

--- src/main/java/representation/SocialEggboxRepresentationV1.java
+++ src/main/java/representation/SocialEggboxRepresentationV1.java
@@ -3,8 +3,22 @@ package representation;
 import javax.ws.rs.core.Response;
 
 public class SocialEggboxRepresentationV1 {
-    public Response getUserById(String userid) {
-        Response.ResponseBuilder builder= Response.status(200).entity(new User("email","name","facebookId"));
+    UserDAO userRepository;
+
+    public SocialEggboxRepresentationV1(UserDAO userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public Response getUserById(String userId) {
+        User user = userRepository.read(userId);
+        Response.ResponseBuilder builder;
+        if (user != null){
+            builder= Response.status(200).entity(new User("email","name","facebookId"));
+        }
+        else {
+            builder= Response.status(404).entity(null);
+        }
+
         return builder.build();
     }
 }
--- test/main/java/representation/SocialEggboxRepresentationTest.java
+++ test/main/java/representation/SocialEggboxRepresentationTest.java
@@ -6,26 +6,43 @@ import org.junit.Test;
 import javax.ws.rs.core.Response;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
 public class SocialEggboxRepresentationTest {
     SocialEggboxRepresentationV1 representation;
 
     @Before
     public void setup(){
-        representation = new SocialEggboxRepresentationV1();
+        User user = new User("email","name","facebookId");
+        UserDAO repo = new UserDAO();
+        repo.save(user);
+
+        representation = new SocialEggboxRepresentationV1(repo);
     }
 
     @Test
     public void getUserByIdWhichExistsShouldReturn200Status(){
-        Response response = representation.getUserById("userid");
+        Response response = representation.getUserById("email");
         assertEquals(200, response.getStatus());
     }
 
     @Test
     public void getUserByIdWhichExistsShouldReturnUserInBody(){
-        Response response = representation.getUserById("userid");
+        Response response = representation.getUserById("email");
         User user = new User("email","name","facebookId");
         assertEquals(user, response.getEntity());
+    }
+
+    @Test
+    public void getUserByIdShouldReturn404IfNotFound(){
+        Response response = representation.getUserById("unknownUserId");
+        assertEquals(404, response.getStatus());
+    }
+
+    @Test
+    public void getUserByIdShouldReturnEmptyBodyIfNotFound(){
+        Response response = representation.getUserById("unknownUserId");
+        assertNull(response.getEntity());
     }
 
 }
