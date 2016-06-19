6e64ec904474a6367492b95e61ac0161cb1d1168
derek clark <derekclark14@googlemail.com>
2016-06-19 10:05:33 +0100

    adds User equality

--- src/main/java/representation/User.java
+++ src/main/java/representation/User.java
@@ -19,7 +19,31 @@ public class User {
         this.email = email;
         this.name = name;
         this.facebookId = facebookId;
-
-
     }
+
+
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj){
+            return true;
+        }
+        if ((obj == null) || (obj.getClass() != this.getClass())){
+            return false;
+        }
+        User user = (User) obj;
+
+        if (! this.email.equals(user.getEmail())) return false;
+        if (! this.name.equals(user.getName())) return false;
+        if (! this.facebookId.equals(user.getFacebookId())) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode(){
+        int hash = 7;
+        hash = 31 * hash + email.hashCode() + name.hashCode() + facebookId.hashCode();
+        return hash;
+    }
+
 }
--- test/main/java/representation/UserTest.java
+++ test/main/java/representation/UserTest.java
@@ -3,6 +3,7 @@ package representation;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class UserTest {
 
@@ -13,4 +14,26 @@ public class UserTest {
         assertEquals("name", user.getName());
         assertEquals("facebookId", user.getFacebookId());
     }
+
+    @Test
+    public void shouldBeEqual(){
+        User user1 = new User("email","name","facebookId");
+        User user2 = new User("email","name","facebookId");
+        assertEquals(user1, user2);
+    }
+
+    @Test
+    public void shouldNotBeEqual(){
+        User user1 = new User("email","name","facebookId");
+        User differentEmail = new User("differentEmail","name","facebookId");
+        assertNotEquals(user1, differentEmail);
+
+        User differentName = new User("email","differentName","facebookId");
+        assertNotEquals(user1, differentName);
+
+        User differentFacebookId = new User("email","name","differentFacebookId");
+        assertNotEquals(user1, differentFacebookId);
+
+    }
+
 }
