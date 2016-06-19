dc1035b9971bd33e85460095f06c267bdf95d2c7
derek clark <derekclark14@googlemail.com>
2016-06-19 10:06:40 +0100

    get by user id returns user entity in body

--- test/main/java/representation/SocialEggboxRepresentationTest.java
+++ test/main/java/representation/SocialEggboxRepresentationTest.java
@@ -1,5 +1,6 @@
 package representation;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import javax.ws.rs.core.Response;
@@ -7,10 +8,24 @@ import javax.ws.rs.core.Response;
 import static org.junit.Assert.assertEquals;
 
 public class SocialEggboxRepresentationTest {
+    SocialEggboxRepresentationV1 representation;
+
+    @Before
+    public void setup(){
+        representation = new SocialEggboxRepresentationV1();
+    }
+
     @Test
     public void getUserByIdWhichExistsShouldReturn200Status(){
-        SocialEggboxRepresentationV1 representation = new SocialEggboxRepresentationV1();
         Response response = representation.getUserById("userid");
         assertEquals(200, response.getStatus());
     }
+
+    @Test
+    public void getUserByIdWhichExistsShouldReturnUserInBody(){
+        Response response = representation.getUserById("userid");
+        User user = new User("email","name","facebookId");
+        assertEquals(user, response.getEntity());
+    }
+
 }
