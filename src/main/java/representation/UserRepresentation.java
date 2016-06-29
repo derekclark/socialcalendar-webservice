b2d9ff63290605e089f3c99de35d2e4ec6c98c82
derek clark <derekclark14@googlemail.com>
2016-06-29 12:27:42 +0100

    marshalls user object to json

--- src/main/java/representation/UserRepresentation.java
+++ src/main/java/representation/UserRepresentation.java
@@ -1,23 +1,13 @@
 package representation;
 
 public class UserRepresentation {
-    private String email, name, facebookId;
+    private User user;
 
-    public UserRepresentation(String email, String name, String facebookId) {
-        this.email = email;
-        this.name = name;
-        this.facebookId = facebookId;
+    public UserRepresentation(User user) {
+        this.user = user;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getFacebookId() {
-        return facebookId;
+    public User getUser() {
+        return user;
     }
 }
--- test/main/java/representation/UserRepresentationTest.java
+++ test/main/java/representation/UserRepresentationTest.java
@@ -1,15 +1,49 @@
 package representation;
 
+import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.SerializationConfig;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
 public class UserRepresentationTest {
+    public static final String EMAIL = "email";
+    public static final String NAME = "name";
+    public static final String FACEBOOK_ID = "facebookId";
+
+    User user = new User(EMAIL, NAME, FACEBOOK_ID);
+    UserRepresentation representation;
+
+    @Before
+    public void setup(){
+        representation = new UserRepresentation(user);
+    }
+
     @Test
     public void canCreateRepresentation(){
-        UserRepresentation representation = new UserRepresentation("email","name","facebookId");
-        assertEquals("email",representation.getEmail());
-        assertEquals("name",representation.getName());
-        assertEquals("facebookId",representation.getFacebookId());
+        assertEquals(user,representation.getUser());
     }
+
+    @Test
+    public void canMarshallToJson() throws IOException {
+        String expectedJson = "{\n" +
+                "  \"user\" : {\n" +
+                "    \"email\" : \"email\",\n" +
+                "    \"name\" : \"name\",\n" +
+                "    \"facebookId\" : \"facebookId\"\n" +
+                "  }\n" +
+                "}";
+
+        assertEquals(expectedJson, getJsonString(representation));
+    }
+
+    private String getJsonString (Object object) throws IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationConfig.Feature.INDENT_OUTPUT);
+        return objectMapper.writeValueAsString(object);
+    }
+
 }
