1bb6371f330d589976b38435d3d942ace586160b
derek clark <derekclark14@googlemail.com>
2016-07-01 17:13:50 +0100

    cleans up bdd tests

--- src/bddtest/java/uk/co/socialeggbox/stepdefs/GetUserById.java
+++ src/bddtest/java/uk/co/socialeggbox/stepdefs/GetUserById.java
@@ -1,5 +1,6 @@
 package uk.co.socialeggbox.stepdefs;
 
+import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
@@ -14,16 +15,18 @@ public class GetUserById {
     public static final String URL = "http://localhost:9000/social/v1/user/";
     Response response;
 
-//    @After
-//    public void teardown(){
-//        response = new SimpleHttpClient().delete(URL+"decla");
-//        assertEquals(200, response.getStatus());
-//    }
+    @After
+    public void teardown(){
+        response = new SimpleHttpClient().delete(URL+"decla");
+
+        if (response.getStatus() == 200){
+            response = new SimpleHttpClient().delete(URL+"decla");
+        }
+    }
 
     @Given("^user exists with email \"(.*?)\", name \"(.*?)\" and facebookId \"(.*?)\"$")
     public void user_exists_with_email_name_and_facebookId(String email,
                                                            String name, String facebookId) throws Throwable {
-        System.out.println("writing user with id="+email);
         response = new SimpleHttpClient().post(URL,
                 new JsonUtility().toJson(new User(email, name, facebookId)));
         assertEquals(200, response.getStatus());
@@ -35,22 +38,19 @@ public class GetUserById {
     }
 
 
-    @Then("^the payload is returned with email \"(.*?)\", name \"(.*?)\", facebookId \"(.*?)\" and a http status of (\\d+)$")
-    public void the_payload_is_returned_with_email_name_facebookId_and_a_http_status_of(String expectedEmail,
-                         String expectedName, String expectedFacebookId, int expectedHttpStatus) throws Throwable {
+    @Then("^the payload is returned with email \"(.*?)\", name \"(.*?)\", facebookId \"(.*?)\"$")
+    public void the_payload_is_returned_with_email_name_facebookId(String email, String name, String facebookId) throws Throwable {
         String expectedJson = "{\n" +
-                "  \"email\" : \"decla\",\n" +
-                "  \"name\" : \"derek\",\n" +
-                "  \"facebookId\" : \"1234\"\n" +
+                "  \"email\" : \""+email+"\",\n" +
+                "  \"name\" : \""+name+"\",\n" +
+                "  \"facebookId\" : \""+facebookId+"\"\n" +
                 "}";
         assertEquals(expectedJson,response.getBody());
-        assertEquals(response.getStatus(), expectedHttpStatus);
     }
 
     @When("^a request is made to delete user with email \"(.*?)\"$")
     public void a_request_is_made_to_get_delete_user_with_email(String email) throws Throwable {
         String url = URL + email;
-        System.out.println("delete url="+url);
         response = new SimpleHttpClient().delete(url);
     }
 
--- src/bddtest/resources/features/GetUserById.feature
+++ src/bddtest/resources/features/GetUserById.feature
@@ -3,10 +3,11 @@ Feature: Get User By Id
   Scenario: Save and Get User By Id
     Given user exists with email "decla", name "derek" and facebookId "1234"
     When a request is made to get user with email "decla"
-    Then the payload is returned with email "decla", name "derek", facebookId "1234" and a http status of 200
+    Then a http code status of 200 is returned
+    Then the payload is returned with email "decla", name "derek", facebookId "1234"
 
   Scenario: Delete a user
-    Given user exists with email "decla1", name "derek" and facebookId "1234"
-    When a request is made to delete user with email "decla1"
+    Given user exists with email "decla", name "derek" and facebookId "1234"
+    When a request is made to delete user with email "decla"
     Then a http code status of 200 is returned
-    And the user with email "decla1" does not exist
+    And the user with email "decla" does not exist
