05f43f2582e699bfb43a146e8d6a55bb4a11ebe0
derek clark <derekclark14@googlemail.com>
2016-07-28 20:22:34 +0100

    tidies up test

--- test/main/java/representation/AvailabilityRepresentationTest.java
+++ test/main/java/representation/AvailabilityRepresentationTest.java
@@ -5,18 +5,19 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
 public class AvailabilityRepresentationTest {
+    public static final String TITLE = "title";
+    public static final String EMAIL = "email";
+    public static final String NAME = "name";
+    public static final String STATUS = "status";
 
     @Test
     public void returnsAvailability(){
-        AvailabilityRepresentation representation = new AvailabilityRepresentation("title", "email",
-                "name",
-                "status");
+        AvailabilityRepresentation representation = new AvailabilityRepresentation(TITLE, EMAIL,
+                NAME, STATUS);
 
         Availability availability = representation.asAvailability();
-        Availability expectedAvailability = new Availability("title",
-                "email",
-                "name",
-                "status");
+        Availability expectedAvailability = new Availability(TITLE,
+                EMAIL,NAME, STATUS);
         assertEquals(expectedAvailability, availability);
     }
 }
