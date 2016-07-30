ac3abf21a19502cb8ee08427126f011dfc25990f
derek clark <derekclark14@googlemail.com>
2016-07-30 09:02:57 +0100

    add equality checks

--- src/main/java/representation/AvailabilityRepresentation.java
+++ src/main/java/representation/AvailabilityRepresentation.java
@@ -37,4 +37,30 @@ public class AvailabilityRepresentation {
     public Availability asAvailability() {
         return new Availability(title, ownerEmail, ownerName, status);
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        AvailabilityRepresentation that = (AvailabilityRepresentation) o;
+
+        if (id != that.id) return false;
+        if (ownerEmail != null ? !ownerEmail.equals(that.ownerEmail) : that.ownerEmail != null) return false;
+        if (ownerName != null ? !ownerName.equals(that.ownerName) : that.ownerName != null) return false;
+        if (status != null ? !status.equals(that.status) : that.status != null) return false;
+        if (title != null ? !title.equals(that.title) : that.title != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (title != null ? title.hashCode() : 0);
+        result = 31 * result + (ownerEmail != null ? ownerEmail.hashCode() : 0);
+        result = 31 * result + (ownerName != null ? ownerName.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        return result;
+    }
 }
--- test/main/java/representation/AvailabilityRepresentationTest.java
+++ test/main/java/representation/AvailabilityRepresentationTest.java
@@ -1,20 +1,27 @@
 package representation;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 public class AvailabilityRepresentationTest {
     public static final String TITLE = "title";
     public static final String EMAIL = "email";
     public static final String NAME = "name";
     public static final String STATUS = "status";
+    AvailabilityRepresentation representation;
+
+    @Before
+    public void setup(){
+        representation = new AvailabilityRepresentation(TITLE, EMAIL,
+                NAME, STATUS);
+
+    }
 
     @Test
     public void returnsAvailability(){
-        AvailabilityRepresentation representation = new AvailabilityRepresentation(TITLE, EMAIL,
-                NAME, STATUS);
-
         Availability availability = representation.asAvailability();
         Availability expectedAvailability = new Availability(TITLE,
                 EMAIL,NAME, STATUS);
@@ -26,6 +33,33 @@ public class AvailabilityRepresentationTest {
         Availability availability = new Availability(TITLE, EMAIL, NAME, STATUS);
         AvailabilityRepresentation availabilityRepresentation = new AvailabilityRepresentation(availability);
         assertEquals(availability, availabilityRepresentation.asAvailability());
+    }
+
+    @Test
+    public void shouldBeEqual(){
+        AvailabilityRepresentation sameContent = new AvailabilityRepresentation(TITLE, EMAIL,
+                NAME, STATUS);
+        assertEquals(sameContent, representation);
+        assertEquals(representation, representation);
+    }
+
+    @Test
+    public void shouldNotBeEqual(){
+        AvailabilityRepresentation differentTitle = new AvailabilityRepresentation("", EMAIL,
+                NAME, STATUS);
+        AvailabilityRepresentation differentEmail = new AvailabilityRepresentation(TITLE, "",
+                NAME, STATUS);
+        AvailabilityRepresentation differentName = new AvailabilityRepresentation(TITLE, EMAIL,
+                "", STATUS);
+        AvailabilityRepresentation differentStatus = new AvailabilityRepresentation(TITLE, EMAIL,
+                NAME, "");
+
+        assertNotEquals(differentTitle, representation);
+        assertNotEquals(differentEmail, representation);
+        assertNotEquals(differentName, representation);
+        assertNotEquals(differentStatus, representation);
+        assertNotEquals("", representation);
+        assertNotEquals(null, representation);
     }
 
 }
