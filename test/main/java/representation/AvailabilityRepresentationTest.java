0371a8bcdc04115bb29b44e9681c8923dd187d14
derek clark <derekclark14@googlemail.com>
2016-07-30 14:04:12 +0100

    tests availability representation produces expected json

--- test/main/java/representation/AvailabilityRepresentationTest.java
+++ test/main/java/representation/AvailabilityRepresentationTest.java
@@ -2,7 +2,9 @@ package representation;
 
 import org.junit.Before;
 import org.junit.Test;
+import utilities.JsonUtility;
 
+import java.io.IOException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -15,6 +17,7 @@ public class AvailabilityRepresentationTest {
     public static final String NAME = "name";
     public static final String STATUS = "status";
     public static final String FACEBOOK_ID = "facebookId";
+    public static final String NEWLINE = "\n";
     AvailabilityRepresentation representation;
 
     Set<User> sharedList = new HashSet<>();
@@ -73,6 +76,23 @@ public class AvailabilityRepresentationTest {
         assertNotEquals(differentSharedUsers, representation);
         assertNotEquals("", representation);
         assertNotEquals(null, representation);
+    }
+
+    @Test
+    public void testJsonRepresentation() throws IOException {
+        String expectedJson = "{" + NEWLINE +
+                "  \"id\" : 0," + NEWLINE +
+                "  \"title\" : \""+TITLE+"\"," + NEWLINE +
+                "  \"ownerEmail\" : \""+EMAIL+"\"," + NEWLINE +
+                "  \"ownerName\" : \""+NAME+"\"," + NEWLINE +
+                "  \"status\" : \""+STATUS+"\"," + NEWLINE +
+                "  \"sharedWithUsers\" : [ ]"+ NEWLINE +
+                "}";
+
+        Availability availability = new Availability(TITLE, EMAIL, NAME, STATUS, sharedList);
+        AvailabilityRepresentation representation = new AvailabilityRepresentation(availability);
+        String actualJson = new JsonUtility().toJson(representation);
+        assertEquals(expectedJson, actualJson);
     }
 
 }
