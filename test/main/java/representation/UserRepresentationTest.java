9c0bb1c56daf2c9125f9ddd5243dea7093a8ebbd
derek clark <derekclark14@googlemail.com>
2016-07-30 14:09:35 +0100

    tests user representation produces expected json format

--- test/main/java/representation/UserRepresentationTest.java
+++ test/main/java/representation/UserRepresentationTest.java
@@ -1,27 +1,47 @@
 package representation;
 
+import org.junit.Before;
 import org.junit.Test;
+import utilities.JsonUtility;
+
+import java.io.IOException;
 
 import static org.junit.Assert.assertEquals;
 
 public class UserRepresentationTest {
-
     public static final String EMAIL = "email";
     public static final String NAME = "name";
     public static final String FACEBOOK_ID = "facebookId";
+    public static final String NEWLINE = "\n";
+
+    User user;
+
+    @Before
+    public void setup(){
+        user = new User(EMAIL, NAME, FACEBOOK_ID);
+    }
 
     @Test
     public void returnsUser(){
         UserRepresentation userRepresentation = new UserRepresentation(EMAIL, NAME, FACEBOOK_ID);
-        User user = new User(EMAIL, NAME, FACEBOOK_ID);
         assertEquals(user,userRepresentation.asUser());
     }
 
     @Test
     public void buildRepresentationFromUserObject(){
-        User user = new User(EMAIL, NAME, FACEBOOK_ID);
         UserRepresentation userRepresentation = new UserRepresentation(user);
         assertEquals(user, userRepresentation.asUser());
     }
 
+    @Test
+    public void testsJsonRepresentation() throws IOException {
+        String expectedJson = "{" + NEWLINE +
+                "  \"email\" : \""+EMAIL+"\"," + NEWLINE +
+                "  \"name\" : \""+NAME+"\"," + NEWLINE +
+                "  \"facebookId\" : \""+FACEBOOK_ID+"\"" + NEWLINE +
+                "}";
+        UserRepresentation representation = new UserRepresentation(user);
+        String actualJson = new JsonUtility().toJson(representation);
+        assertEquals(expectedJson, actualJson);
+    }
 }
