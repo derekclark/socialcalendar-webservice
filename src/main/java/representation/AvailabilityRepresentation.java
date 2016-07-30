31408625025a540f650d28bd93593a07e323c2b1
derek clark <derekclark14@googlemail.com>
2016-07-30 10:57:34 +0100

    adds shared users attribute to availability representation

--- src/main/java/representation/AvailabilityRepresentation.java
+++ src/main/java/representation/AvailabilityRepresentation.java
@@ -3,6 +3,8 @@ package representation;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Set;
+
 public class AvailabilityRepresentation {
     @JsonProperty("id")
     private int id;
@@ -14,6 +16,8 @@ public class AvailabilityRepresentation {
     private String ownerName;
     @JsonProperty("status")
     private String status;
+    @JsonProperty("sharedWithUsers")
+    private Set<User> sharedWithUsers;
 
     @JsonCreator
     public AvailabilityRepresentation(@JsonProperty("title")String title,
@@ -26,12 +30,26 @@ public class AvailabilityRepresentation {
         this.status = status;
     }
 
+    @JsonCreator
+    public AvailabilityRepresentation(@JsonProperty("title")String title,
+                                      @JsonProperty("ownerEmail")String ownerEmail,
+                                      @JsonProperty("ownerName")String ownerName,
+                                      @JsonProperty("status")String status,
+                                      @JsonProperty("sharedWithUsers") Set<User> sharedWithUsers) {
+        this.title = title;
+        this.ownerEmail = ownerEmail;
+        this.ownerName = ownerName;
+        this.status = status;
+        this.sharedWithUsers = sharedWithUsers;
+    }
+
     public AvailabilityRepresentation(Availability availability){
         this.id = availability.getId();
         this.title = availability.getTitle();
         this.ownerEmail = availability.getOwnerEmail();
         this.ownerName = availability.getOwnerName();
         this.status = availability.getStatus();
+        this.sharedWithUsers = availability.getSharedWithUsers();
     }
 
     public Availability asAvailability() {
@@ -48,6 +66,8 @@ public class AvailabilityRepresentation {
         if (id != that.id) return false;
         if (ownerEmail != null ? !ownerEmail.equals(that.ownerEmail) : that.ownerEmail != null) return false;
         if (ownerName != null ? !ownerName.equals(that.ownerName) : that.ownerName != null) return false;
+        if (sharedWithUsers != null ? !sharedWithUsers.equals(that.sharedWithUsers) : that.sharedWithUsers != null)
+            return false;
         if (status != null ? !status.equals(that.status) : that.status != null) return false;
         if (title != null ? !title.equals(that.title) : that.title != null) return false;
 
@@ -61,6 +81,7 @@ public class AvailabilityRepresentation {
         result = 31 * result + (ownerEmail != null ? ownerEmail.hashCode() : 0);
         result = 31 * result + (ownerName != null ? ownerName.hashCode() : 0);
         result = 31 * result + (status != null ? status.hashCode() : 0);
+        result = 31 * result + (sharedWithUsers != null ? sharedWithUsers.hashCode() : 0);
         return result;
     }
 
--- test/main/java/representation/AvailabilityRepresentationTest.java
+++ test/main/java/representation/AvailabilityRepresentationTest.java
@@ -3,6 +3,9 @@ package representation;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
@@ -11,13 +14,15 @@ public class AvailabilityRepresentationTest {
     public static final String EMAIL = "email";
     public static final String NAME = "name";
     public static final String STATUS = "status";
+    public static final String FACEBOOK_ID = "facebookId";
     AvailabilityRepresentation representation;
+
+    Set<User> sharedList = new HashSet<>();
 
     @Before
     public void setup(){
-        representation = new AvailabilityRepresentation(TITLE, EMAIL,
-                NAME, STATUS);
-
+        Availability availability = new Availability(TITLE, EMAIL, NAME, STATUS, sharedList);
+        representation = new AvailabilityRepresentation(availability);
     }
 
     @Test
@@ -38,7 +43,7 @@ public class AvailabilityRepresentationTest {
     @Test
     public void shouldBeEqual(){
         AvailabilityRepresentation sameContent = new AvailabilityRepresentation(TITLE, EMAIL,
-                NAME, STATUS);
+                NAME, STATUS, sharedList);
         assertEquals(sameContent, representation);
         assertEquals(representation, representation);
     }
@@ -46,18 +51,26 @@ public class AvailabilityRepresentationTest {
     @Test
     public void shouldNotBeEqual(){
         AvailabilityRepresentation differentTitle = new AvailabilityRepresentation("", EMAIL,
-                NAME, STATUS);
+                NAME, STATUS, sharedList);
         AvailabilityRepresentation differentEmail = new AvailabilityRepresentation(TITLE, "",
-                NAME, STATUS);
+                NAME, STATUS, sharedList);
         AvailabilityRepresentation differentName = new AvailabilityRepresentation(TITLE, EMAIL,
-                "", STATUS);
+                "", STATUS, sharedList);
         AvailabilityRepresentation differentStatus = new AvailabilityRepresentation(TITLE, EMAIL,
-                NAME, "");
+                NAME, "", sharedList);
+
+        Set<User> differentSharedList = new HashSet<User>();
+        differentSharedList.add(new User(EMAIL, NAME, FACEBOOK_ID));
+        differentSharedList.add(new User("another email", NAME, FACEBOOK_ID));
+
+        AvailabilityRepresentation differentSharedUsers = new AvailabilityRepresentation(TITLE, EMAIL,
+                NAME, STATUS, differentSharedList);
 
         assertNotEquals(differentTitle, representation);
         assertNotEquals(differentEmail, representation);
         assertNotEquals(differentName, representation);
         assertNotEquals(differentStatus, representation);
+        assertNotEquals(differentSharedUsers, representation);
         assertNotEquals("", representation);
         assertNotEquals(null, representation);
     }
